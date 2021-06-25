d409d8a6e151e3af72871769d180a8eb136924d0
Brayan Esneider <brayanhernandezmartinez@gmail.com>
2021-06-24 21:00:04 -0500

    Se realiza un ejercicio de nivel bajo-medio llamado Math Issues

---
+++ Math Issues/Math_Issues.js
@@ -0,0 +1,27 @@
+Math.round = function(number) {
+    let aux = 0;
+    for(let i = 0; i <= number;i++){
+        aux = i;
+    }
+    return number-aux >= 0.5 ? aux+1 : aux; // TODO: fix this
+  };
+  
+  Math.ceil = function(number) {
+      let aux = 0;
+    for(let i = 0; i <= number;i++){
+        aux = i;
+    }
+    return number-aux == 0 ? aux : aux+1;
+  };
+  
+  Math.floor = function(number) {
+      let aux = 0;
+    for(let i = 0; i <= number;i++){
+        aux = i;
+    }
+    return aux;
+  };
+
+  console.log(Math.round(0.5));
+  console.log(Math.ceil(0.5));
+  console.log(Math.floor(0.5));
